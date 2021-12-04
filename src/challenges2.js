eed988ae2f0dd7145b45e558fdc4768d745c080c
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2021-12-04 13:15:44 -0300

    Requisito 10 - corrigido

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,7 +3,7 @@ function techList(tecnologias, name) {
   let novoArray = [];
   let ordenado = tecnologias.sort();
   if (tecnologias.length > 0) {
-    for (let index = 0; index < ordenado.length; index++) {
+    for (let index = 0; index < ordenado.length; index=+ 1) {
       novoArray.push({
         tech: ordenado[index],
         name: name,
